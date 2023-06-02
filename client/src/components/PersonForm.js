ba809cb2da5198a35ade99424f9c4ed6d091c430
Joel Jacobson <joel.e.jacobson@gmail.com>
2023-06-02 13:12:09 -0600

    completed front end setup

--- client/src/components/PersonForm.js
+++ client/src/components/PersonForm.js
@@ -1,16 +1,43 @@
-import React, { useEffect, useState} from 'react'
+import React, { useState } from 'react'
 import axios from 'axios'
 const PersonForm = () => {
-    const [ message, setMessage ] = useState('Loading...')
-    useEffect(() => {
-        axios.get('http://localhost:8000/api')
-            .then(res => setMessage(res.data.message))
+    // const [ message, setMessage ] = useState('Loading...')
+    // useEffect(() => {
+    //     axios.get('http://localhost:8000/api')
+    //         .then(res => setMessage(res.data.message))
+    //         .catch(err => console.log(err))
+    // }, [])
+    // keep track of what is being typed via useState hook
+    const [firstName, setFirstName] = useState("")
+    const [lastName, setLastName] = useState("")
+
+    // handle the submmited form
+    const onSubmitHandler = (e) => {
+        // prevent default behavior
+        e.preventDefault()
+        // make post request to create a new person
+        axios.post('http://localhost:8000/api/people', {
+            firstName,  // no need to type firstname: firstname
+            lastName
+        })
+            .then(res => {
+                console.log(res)
+                console.log(res.data)
+            })
             .catch(err => console.log(err))
-    }, [])
+    }
     return (
-        <div>
-            <h2>Message from the backend: {message}</h2>
-        </div>
+        <form onSubmit={ onSubmitHandler }>
+            <p>
+                <label htmlFor="firstName">First Name</label>
+                <input type="text" id="firstName" onChange = { (e) => setFirstName(e.target.value)}/>
+            </p>
+            <p>
+                <label htmlFor="lastName">Last Name</label>
+                <input type="text" id="lastName" onChange = { (e) => setLastName(e.target.value) }/>
+            </p>
+            <input type="submit" />
+        </form>
     )
 }
 
